c3bc37bee5a1b253e025f1ae61c46190fd567dc1
caleb bird <caleb.bird123@gmail.com>
2023-03-17 12:03:13 -0600

    giRevert "generated scaffold combatant"
    This reverts commit 4b8a61c76974ba32d72f5951dcff5d679c094039.

--- test/controllers/combatants_controller_test.rb
+++ test/controllers/combatants_controller_test.rb
@@ -17,7 +17,7 @@ class CombatantsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create combatant" do
     assert_difference("Combatant.count") do
-      post combatants_url, params: { combatant: { AC: @combatant.AC, Cha_Save: @combatant.Cha_Save, Con_Save: @combatant.Con_Save, Dead: @combatant.Dead, Death_Save_Fail: @combatant.Death_Save_Fail, Death_Save_Pass: @combatant.Death_Save_Pass, Dex_Save: @combatant.Dex_Save, HP: @combatant.HP, HP_Max: @combatant.HP_Max, Initiative: @combatant.Initiative, Int_Save: @combatant.Int_Save, Name: @combatant.Name, Str_Save: @combatant.Str_Save, Temp_HP: @combatant.Temp_HP, Wis_Save: @combatant.Wis_Save } }
+      post combatants_url, params: { combatant: { AC: @combatant.AC, Cha_Save: @combatant.Cha_Save, Con_Save: @combatant.Con_Save, Death_Save_Fail: @combatant.Death_Save_Fail, Death_Save_Pass: @combatant.Death_Save_Pass, Dex_Save: @combatant.Dex_Save, HP: @combatant.HP, HP_Max: @combatant.HP_Max, Initiative: @combatant.Initiative, Int_save: @combatant.Int_save, Name: @combatant.Name, Str_Save: @combatant.Str_Save, Temp_HP: @combatant.Temp_HP, Wis_save: @combatant.Wis_save } }
     end
 
     assert_redirected_to combatant_url(Combatant.last)
@@ -34,7 +34,7 @@ class CombatantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update combatant" do
-    patch combatant_url(@combatant), params: { combatant: { AC: @combatant.AC, Cha_Save: @combatant.Cha_Save, Con_Save: @combatant.Con_Save, Dead: @combatant.Dead, Death_Save_Fail: @combatant.Death_Save_Fail, Death_Save_Pass: @combatant.Death_Save_Pass, Dex_Save: @combatant.Dex_Save, HP: @combatant.HP, HP_Max: @combatant.HP_Max, Initiative: @combatant.Initiative, Int_Save: @combatant.Int_Save, Name: @combatant.Name, Str_Save: @combatant.Str_Save, Temp_HP: @combatant.Temp_HP, Wis_Save: @combatant.Wis_Save } }
+    patch combatant_url(@combatant), params: { combatant: { AC: @combatant.AC, Cha_Save: @combatant.Cha_Save, Con_Save: @combatant.Con_Save, Death_Save_Fail: @combatant.Death_Save_Fail, Death_Save_Pass: @combatant.Death_Save_Pass, Dex_Save: @combatant.Dex_Save, HP: @combatant.HP, HP_Max: @combatant.HP_Max, Initiative: @combatant.Initiative, Int_save: @combatant.Int_save, Name: @combatant.Name, Str_Save: @combatant.Str_Save, Temp_HP: @combatant.Temp_HP, Wis_save: @combatant.Wis_save } }
     assert_redirected_to combatant_url(@combatant)
   end
 
