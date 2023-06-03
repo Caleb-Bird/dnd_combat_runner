ce041e9064ec209d2f9f3b3ada9aae0731792e9d
caleb bird <caleb.bird123@gmail.com>
2023-06-03 10:54:11 -0600

    useimmer and state

--- app/javascript/application/components/CombatRun.jsx
+++ app/javascript/application/components/CombatRun.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
 import CombatantManager from './CombatantManager'
+import { useImmer } from "use-immer";
 
-const CombatRun = (props) => (
-  <>
+
+
+const CombatRun = (props) => {
+  const [combatState, updateCombatState] = useImmer({
+    selectedCombatantInCombat: null,
+    ...props
+  });
+
+  const updateSelectedCombatantInCombat = (selectedCombatantInCombat)=>{
+    updateCombatState(combatState =>{
+      combatState.selectedCombatantInCombatant = selectedCombatantInCombat;
+    });
+  
+  }
+  return <>
     <div className="container">
       <div className="row">
         <div className="col">
-          <CombatantManager {...props}> </CombatantManager>
-        </div> 
+          <CombatantManager 
+            combatants_in_combat={combatState.combatants_in_combat}
+            selectedCombatantInCombat = {combatState.selectedCombatantInCombat}
+            updateCombatState = {updateCombatState}
+            updateSelectedCombatantInCombat ={updateSelectedCombatantInCombat}
+          />
+        </div>
         <div className="col">
         </div>
       </div>
     </div>
   </>
-)
-
+}
 export default CombatRun
--- app/javascript/application/components/CombatantManager.jsx
+++ app/javascript/application/components/CombatantManager.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
+
 const CombatantManager = (props) => {
+
+  const calculateTablePrimary = (combatant_in_combat)=>{
+    if (!props.selectedCombatantInCombat){return '';}
+    if (combatant_in_combat.id == props.selectedCombatantInCombat.id) {
+      return 'table-primary'
+    }
+    return '';
+  } 
   return (
     <>
     <table className="table table-hover table-bordered">
@@ -13,8 +22,10 @@ const CombatantManager = (props) => {
         </tr>
       </thead>
       <tbody>
-      {props.combatants_in_combat.sort((cic1,cic2)=>(cic2.working_initiative-cic1.working_initiative)).map((combatant_in_combat, index) => (
-          <tr key={index}>
+      {[...props.combatants_in_combat]
+        .sort((cic1,cic2)=>(cic2.working_initiative-cic1.working_initiative))
+        .map((combatant_in_combat, index) => (
+          <tr className={calculateTablePrimary(combatant_in_combat)} key={index}>
             <td>{combatant_in_combat.combatant.name}</td>
             <td>{combatant_in_combat.current_hp}</td>
             <td>{combatant_in_combat.temporary_hp}</td>
